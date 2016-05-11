558c53b2e4548fe60e8e4178ca35d77cb05746ef
Lance Ball <lball@redhat.com>
2016-05-11 14:40:35 -0400

    Convert specs from jasmine to tape.
    We don't need no stinkin' globals.

--- child-repo/spec/grant-manager-spec.js
+++ child-repo/spec/grant-manager-spec.js
@@ -1,101 +1,92 @@
 'use strict';
 
-/* jshint jasmine: true */
-
 const GrantManager = require('./../index').GrantManager;
 const Config       = require('./../index').Config;
-
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+const test         = require('tape');
 
 const delay = (ms) => (value) => new Promise((resolve) => setTimeout(() => resolve(value), ms));
+const getManager = (fixture) => new GrantManager(new Config(fixture));
 
-describe( "grant manager in public mode", () => {
-  const config       = new Config('spec/fixtures/keycloak-public.json');
-  const manager      = new GrantManager(config);
-
-  it( 'should be able to obtain a grant', (done) => {
-    manager.obtainDirectly( 'test-user', 'tiger' )
-      .then( (grant) => expect( grant.access_token ).not.toBe( undefined ) )
-      .then(done);
-  });
+test( 'GrantManager in public mode should be able to obtain a grant', (t) => {
+  const manager = getManager('spec/fixtures/keycloak-public.json');
+  manager.obtainDirectly( 'test-user', 'tiger' )
+    .then( (grant) => t.notEqual( grant.access_token, undefined ) )
+    .then(t.end);
 });
 
-describe( "grant manager in confidential mode", () => {
-  const config       = new Config('spec/fixtures/keycloak-confidential.json');
-  const manager      = new GrantManager(config);
+const manager = getManager('spec/fixtures/keycloak-confidential.json');
 
-  it( 'should be able to obtain a grant', (done) => {
-    manager.obtainDirectly( 'test-user', 'tiger' )
-      .then( (grant) => expect( grant.access_token ).not.toBe( undefined ) )
-      .then(done);
-  });
+test( 'GrantManager in confidential mode should be able to obtain a grant', (t) => {
+  manager.obtainDirectly( 'test-user', 'tiger' )
+    .then( (grant) => t.notEqual( grant.access_token, undefined ) )
+    .then(t.end);
+});
 
-  it( 'should be able to refresh a grant', (done) => {
-    let originalAccessToken;
-    manager.obtainDirectly( 'test-user', 'tiger' )
-      .then( delay(3000) )
-      .then( (grant) => {
-        expect( grant.access_token ).not.toBe( undefined );
-        originalAccessToken = grant.access_token;
-        return grant;
-      })
-      .then( (grant) => manager.ensureFreshness(grant) )
-      .then( (grant)  => {
-        expect( grant.access_token ).not.toBe( undefined );
-        expect( grant.access_token.token ).not.toBe( originalAccessToken.token );
-      })
-      .then(done);
-  });
+test( 'GrantManager in confidential mode should be able to refresh a grant', (t) => {
+  let originalAccessToken;
+  manager.obtainDirectly( 'test-user', 'tiger' )
+    .then( delay(3000) )
+    .then( (grant) => {
+      t.notEqual( grant.access_token, undefined );
+      originalAccessToken = grant.access_token;
+      return grant;
+    })
+    .then( (grant) => manager.ensureFreshness(grant) )
+    .then( (grant)  => {
+      t.notEqual( grant.access_token, undefined );
+      t.notEqual( grant.access_token.token, originalAccessToken.token );
+    })
+    .then(t.end);
+});
 
-  it( 'should be able to validate a valid token', (done) => {
-    let originalAccessToken;
-    manager.obtainDirectly( 'test-user', 'tiger' )
-      .then( (grant) => {
-        originalAccessToken = grant.access_token;
-        return manager.validateAccessToken( grant.access_token );
-      })
-      .then( (token) => {
-        expect( token ).not.toBe( undefined );
-        expect( token ).toBe( originalAccessToken );
-      })
-      .then(done);
-  });
+test( 'GrantManager in confidential mode should be able to validate a valid token', (t) => {
+  let originalAccessToken;
+  manager.obtainDirectly( 'test-user', 'tiger' )
+    .then( (grant) => {
+      originalAccessToken = grant.access_token;
+      return manager.validateAccessToken( grant.access_token );
+    })
+    .then( (token) => {
+      t.notEqual( token, undefined );
+      t.equal( token, originalAccessToken );
+    })
+    .then(t.end);
+});
 
-  it( 'should be able to validate an invalid token', (done) => {
-    let originalAccessToken;
-    manager.obtainDirectly( 'test-user', 'tiger' )
-      .then( delay(3000) )
-      .then( (grant) => {
-        originalAccessToken = grant.access_token;
-        return manager.validateAccessToken(grant.access_token);
-      })
-      .then( (result) => expect( result ).toBe( false ))
-      .then(done);
-  });
+test( 'GrantManager in confidential mode should be able to validate an invalid token', (t) => {
+  let originalAccessToken;
+  manager.obtainDirectly( 'test-user', 'tiger' )
+    .then( delay(3000) )
+    .then( (grant) => {
+      originalAccessToken = grant.access_token;
+      return manager.validateAccessToken(grant.access_token);
+    })
+    .then( (result) => t.equal( result, false ))
+    .then(t.end);
+});
 
-  it( 'should be able to validate a valid token string', (done) => {
-    let originalAccessToken;
-    manager.obtainDirectly( 'test-user', 'tiger' )
-      .then( (grant) => {
-        originalAccessToken = grant.access_token.token;
-        return manager.validateAccessToken( grant.access_token.token );
-      })
-      .then( (token) => {
-        expect( token ).not.toBe( undefined );
-        expect( token ).toBe( originalAccessToken );
-      })
-      .then(done);
-  });
+test( 'GrantManager in confidential mode should be able to validate a valid token string', (t) => {
+  let originalAccessToken;
+  manager.obtainDirectly( 'test-user', 'tiger' )
+    .then( (grant) => {
+      originalAccessToken = grant.access_token.token;
+      return manager.validateAccessToken( grant.access_token.token );
+    })
+    .then( (token) => {
+      t.notEqual( token, undefined );
+      t.equal( token, originalAccessToken );
+    })
+    .then(t.end);
+});
 
-  it( 'should be able to validate an invalid token string', (done) => {
-    let originalAccessToken;
-    manager.obtainDirectly( 'test-user', 'tiger' )
-      .then( delay(3000) )
-      .then( (grant) => {
-        originalAccessToken = grant.access_token.token;
-        return manager.validateAccessToken( grant.access_token.token );
-      })
-      .then( (result) => expect( result ).toBe( false ))
-      .then(done);
-  });
+test( 'GrantManager in confidential mode should be able to validate an invalid token string', (t) => {
+  let originalAccessToken;
+  manager.obtainDirectly( 'test-user', 'tiger' )
+    .then( delay(3000) )
+    .then( (grant) => {
+      originalAccessToken = grant.access_token.token;
+      return manager.validateAccessToken( grant.access_token.token );
+    })
+    .then( (result) => t.equal( result, false ))
+    .then(t.end);
 });
