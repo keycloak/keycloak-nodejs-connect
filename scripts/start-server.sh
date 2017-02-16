6119bcfea6affa3b8509b7afb906a844adfc3487
Bruno Oliveira <bruno@abstractj.org>
2017-02-16 11:21:30 -0200

    Fix for the CI

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,7 +2,11 @@
 
 . scripts/version.sh
 
+set -v
+
 function waitForServer {
+  # Give the server some time to start up. Look for a well-known
+  # bit of text in the log file. Try at most 50 times before giving up.
   C=50
   while [ $C -gt 0 ]
   do
@@ -19,23 +23,21 @@ function waitForServer {
 }
 
 ARCHIVE="${KEYCLOAK}.tar.gz"
-URL="http://downloads.jboss.org/keycloak/${VERSION}/${ARCHIVE}"
-
-if [ ! -e $ARCHIVE ]
+DIST="keycloak-server-dist"
+URL="https://repo1.maven.org/maven2/org/keycloak/$DIST/${VERSION}/$DIST-${VERSION}.tar.gz"
+# Download keycloak server if we don't already have it
+if [ ! -e $KEYCLOAK ]
 then
-  wget $URL
+  curl -o $ARCHIVE $URL
+  tar xzf $ARCHIVE
+  rm -f $ARCHIVE
 fi
 
-rm -Rf $KEYCLOAK
-tar xzf $ARCHIVE
-sleep 1
+# Start the server
 $KEYCLOAK/bin/standalone.sh -Djava.net.preferIPv4Stack=true \
                             -Dkeycloak.migration.action=import \
                             -Dkeycloak.migration.provider=singleFile \
-                            -Dkeycloak.migration.file=test/keycloak-fixture.json \
+                            -Dkeycloak.migration.file=test/fixtures/keycloak-fixture.json \
                             -Dkeycloak.migration.strategy=OVERWRITE_EXISTING > keycloak.log 2>&1 &
-sleep 1
-waitForServer
-$KEYCLOAK/bin/add-user-keycloak.sh -r master -u admin -p admin
-$KEYCLOAK/bin/jboss-cli.sh --connect command=:reload
+
 waitForServer
