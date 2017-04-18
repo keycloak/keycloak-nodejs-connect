73358bd37666143bae7bcf52a50076026f480bea
Nick Shearer <nick.shearer@software.dell.com>
2017-04-17 21:09:07 -0500

    update promise logic in validateToken / validateGrant to make return result more consistent esp when validation fails add tests to verify grantValidation results

--- child-repo/test/integration/grant-manager-spec.js
+++ child-repo/test/integration/grant-manager-spec.js
@@ -83,6 +83,16 @@ test('GrantManager in confidential mode should be able to validate an invalid to
     .then(t.end);
 });
 
+test('GrantManager in confidential mode should be able to validate a token has an invalid signature', (t) => {
+  manager.obtainDirectly('test-user', 'tiger')
+    .then((grant) => {
+      grant.access_token.token = grant.access_token.token.replace(/(.+?\..+?\.).*/, '$1.InvalidSignatureIsHereAgain');
+      return manager.validateAccessToken(grant.access_token);
+    })
+    .then((result) => t.equal(result, false))
+    .then(t.end);
+});
+
 test('GrantManager in confidential mode should be able to validate a valid token string', (t) => {
   let originalAccessToken;
   manager.obtainDirectly('test-user', 'tiger')
@@ -104,5 +114,39 @@ test('GrantManager in confidential mode should be able to validate an invalid to
       return manager.validateAccessToken(grant.access_token.token);
     })
     .then((result) => t.equal(result, false))
+    .then(t.end);
+});
+
+test('GrantManager should be able to validate tokens in a grant', (t) => {
+  let originalAccessToken, originalRefreshToken, orginalIdToken;
+  manager.obtainDirectly('test-user', 'tiger')
+    .then((grant) => {
+      originalAccessToken = grant.access_token;
+      originalRefreshToken = grant.refresh_token;
+      orginalIdToken = grant.id_token;
+      return manager.validateGrant(grant);
+    })
+    .then((grant) => {
+      t.notEqual(grant.access_token, undefined);
+      t.equal(grant.access_token, originalAccessToken);
+      t.equal(grant.refresh_token, originalRefreshToken);
+      t.equal(grant.id_token, orginalIdToken);
+    })
+    .then(t.end);
+});
+
+test('GrantManager should be able to remove invalid tokens from a grant', (t) => {
+  manager.obtainDirectly('test-user', 'tiger')
+    .then((grant) => {
+      grant.access_token.signature = Buffer.from('this signature is invalid');
+      grant.refresh_token.signature = Buffer.from('this signature is also invalid');
+      grant.id_token.signature = Buffer.from('this signature is still invalid');
+      return manager.validateGrant(grant);
+    })
+    .then((grant) => {
+      t.equal(grant.access_token, undefined);
+      t.equal(grant.refresh_token, undefined);
+      t.equal(grant.id_token, undefined);
+    })
     .then(t.end);
 });
