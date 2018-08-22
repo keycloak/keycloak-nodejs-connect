7f707d5dc29532f13b7500592d19ab9a2accb44f
Bruno Oliveira da Silva <bruno@abstractj.org>
2018-08-22 16:45:54 -0300

    [KEYCLOAK-8116] Change Travis job from Node.js adapter and the quickstart to use Docker

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,37 +1,20 @@
 #!/bin/bash
 
-. scripts/build-keycloak.sh
+# Pull the docker image with the latest changes on Keycloak
+docker pull jboss/keycloak:master
 
-set -v
+# Start the image with default user/admin and import a default realm for integration tests
+docker run -d --name keycloak -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -p 8080:8080 -v `pwd`/test/fixtures/auth-utils/nodejs-test-realm.json:/config/nodejs-test-realm.json -it jboss/keycloak:master -b 0.0.0.0  -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/config/nodejs-test-realm.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING
 
-function waitForServer {
-  # Give the server some time to start up. Look for a well-known
-  # bit of text in the log file. Try at most 50 times before giving up.
-  C=50
-  while [ $C -gt 0 ]
-  do
-    grep "Keycloak ${VERSION} (WildFly Core .*) started" keycloak.log
-    if [ $? -eq 0 ]; then
-      echo "Server started."
-      C=0
-    else
-      echo -n "."
-      C=$(( $C - 1 ))
+# For debugging purposes to make sure the image was started
+counter=0
+printf 'Waiting for Keycloak server to start'
+until $(curl --output /dev/null --silent --head --fail http://localhost:8080/auth/realms/master/.well-known/openid-configuration); do
+    printf '.'
+    sleep 5
+    if [[ "$counter" -gt 10 ]]; then
+      printf "Keycloak server failed to start. Timeout!"
+      exit 1
     fi
-    sleep 1
-  done
-}
-
-KEYCLOAK="keycloak-server"
-
-# Start the server
-$KEYCLOAK/bin/add-user-keycloak.sh -u admin -p admin
-
-# Start the server
-$KEYCLOAK/bin/standalone.sh -Djava.net.preferIPv4Stack=true \
-                            -Dkeycloak.migration.action=import \
-                            -Dkeycloak.migration.provider=singleFile \
-                            -Dkeycloak.migration.file=./test/fixtures/auth-utils/nodejs-test-realm.json \
-                            -Dkeycloak.migration.strategy=OVERWRITE_EXISTING > keycloak.log 2>&1 &
-
-waitForServer
+    counter=$((counter+1))
+done
