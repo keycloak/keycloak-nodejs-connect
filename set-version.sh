1980189ea2940b4addcf246f62cbe4b782b7120d
stianst <stianst@gmail.com>
2018-11-14 20:24:38 +0100

    Prepare for 4.7.0.Final

---
+++ set-version.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+VERSION=$1
+NPM_VERSION=`echo $VERSION | sed 's/.Final//' | sed 's/.CR/-cr./' | sed 's/.Beta/-beta./' | sed 's/.Alpha/-alpha./'`
+
+sed -i 's/"version": .*/"version": "'$NPM_VERSION'"/' package.json
+mvn -f product/pom.xml versions:set -DnewVersion=$NPM_VERSION -DgenerateBackupPoms=false
