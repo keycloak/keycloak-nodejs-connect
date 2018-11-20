54ed09ad3b44f4a856526f4910238cd8faf99ea7
Stian Thorgersen <stianst@gmail.com>
2018-11-20 10:31:22 +0100

    Update set-version.sh

--- set-version.sh
+++ set-version.sh
@@ -3,5 +3,5 @@
 VERSION=$1
 NPM_VERSION=`echo $VERSION | sed 's/.Final//' | sed 's/.CR/-cr./' | sed 's/.Beta/-beta./' | sed 's/.Alpha/-alpha./'`
 
-sed -i 's/"version": .*/"version": "'$NPM_VERSION'"/' package.json
+sed -i 's/"version": .*/"version": "'$NPM_VERSION'",/' package.json
 mvn -f product/pom.xml versions:set -DnewVersion=$NPM_VERSION -DgenerateBackupPoms=false
