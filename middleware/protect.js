b58a9ad31fddef8eaa4b5f92b2854a910ccef1ef
Andrei Rusu <beatfactor@users.noreply.github.com>
2018-12-20 15:16:25 +0100

    use https always when constructing redirect_uri parameter

--- middleware/protect.js
+++ middleware/protect.js
@@ -24,7 +24,7 @@ function forceLogin (keycloak, request, response) {
   let protocol = request.protocol;
   let hasQuery = ~(request.originalUrl || request.url).indexOf('?');
 
-  let redirectUrl = protocol + '://' + host + (port === '' ? '' : ':' + port) + (request.originalUrl || request.url) + (hasQuery ? '&' : '?') + 'auth_callback=1';
+  let redirectUrl = 'https://' + host + (port === '' ? '' : ':' + port) + (request.originalUrl || request.url) + (hasQuery ? '&' : '?') + 'auth_callback=1';
 
   if (request.session) {
     request.session.auth_redirect_uri = redirectUrl;
