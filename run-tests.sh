a71dea163a4818816fe2f4f1276ed29940bdad47
Sebastian Laskawiec <slaskawi@redhat.com>
2019-07-01 12:38:39 +0200

    KEYCLOAK-10187 xUnit report style

--- run-tests.sh
+++ run-tests.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -e
 
+TEST_DIR=test_results
+
 run_tests() {
   FILE_PATH=$1
+  mkdir -p ${TEST_DIR}
 
   echo "------------------------------------"
   echo "Running tests for file: ${FILE_PATH}"
   echo -e "------------------------------------\n"
 
   if [[ -f ${FILE_PATH} ]]; then
+    TEST_NAME="${FILE_PATH##*/}"
 
-    node ${FILE_PATH}
+    node ${FILE_PATH} | tee ${TEST_DIR}/${TEST_NAME}.txt
+    cat ${TEST_DIR}/${TEST_NAME}.txt | node ./node_modules/.bin/tap-xunit --package="${TEST_NAME}" > ${TEST_DIR}/${TEST_NAME}.xml
     RET_VAL=$?
 
     echo "------------------------------------"
@@ -26,7 +31,6 @@ run_tests() {
     exit 2
   fi
 }
-
 
 if [[ "$1" == "group1" ]]; then
   for i in `ls test/*.js | grep -v "enforcer-spec"`; do run_tests ${i}; done
