595776b310434829a84553ddd3daf93bbbfeda5a
Thomas Brüggemann <thomas.brueggemann@wuh-group.com>
2020-12-16 10:05:06 +0100

    Make linter happy

--- middleware/auth-utils/token.js
+++ middleware/auth-utils/token.js
@@ -51,7 +51,7 @@ function Token (token, clientId) {
  *
  * In case of an offline_access refresh token expire value is undefined,
  * so that token will never get expired.
- * 
+ *
  * @return {boolean} `true` if it is expired, otherwise `false`.
  */
 Token.prototype.isExpired = function isExpired () {
