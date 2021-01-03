30fb82d58280af0db5802361e73b8f096c06b89f
Kostas Kapetanakis <kapekost@gmail.com>
2021-01-03 17:46:46 +0000

    fix tests

--- middleware/protect.js
+++ middleware/protect.js
@@ -21,7 +21,7 @@ function forceLogin (keycloak, request, response, next) {
   let headerHost = request.headers.host.split(':');
   let host = request.hostname || headerHost[0];
   let port = headerHost[1] || '';
-  let protocol = request.protocol || request.isSecure() ? 'https' : 'http';
+  let protocol = (request.protocol !== undefined) ? request.protocol : request.isSecure() ? 'https' : 'http';
   let hasQuery = ~(request.originalUrl || request.url).indexOf('?');
 
   let redirectUrl = protocol + '://' + host + (port === '' ? '' : ':' + port) + (request.originalUrl || request.url) + (hasQuery ? '&' : '?') + 'auth_callback=1';
@@ -32,15 +32,14 @@ function forceLogin (keycloak, request, response, next) {
 
   let uuid = UUID();
   let loginURL = keycloak.loginUrl(uuid, redirectUrl);
-  // try {
-    // expressjs not happy with next param
-    response.redirect(loginURL);
-  // } catch (ex) {
-  //   console.log('using Restify? expected error. Switching redirect call ..');
-  //   // for restify mandatory next param
-  //   response.redirect(loginURL, next);
-  //   response.redirect(301, loginURL, next);
-  // }
+  try {
+    expressjs not happy with next param
+    response.redirect(301, loginURL);
+  } catch (ex) {
+    console.log('using Restify? expected error. Switching redirect call ..');
+    // for restify mandatory next param
+    response.redirect(301, loginURL, next);
+  }
 }
 
 function simpleGuard (role, token) {
