37db04c06ec23a0675330e3da3e0cba6ac6b6388
mposolda <mposolda@gmail.com>
2025-01-23 10:09:14 +0100

    Clean up old release code from Node.js adapter repo closes #552
    Signed-off-by: mposolda <mposolda@gmail.com>

--- get-version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-awk '/"version":/ { print $2 }' package.json | sed 's/"//g' | sed 's/,//'
--- set-version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-VERSION=$1
-NPM_VERSION=`echo $VERSION | sed 's/.Final//' | sed 's/.CR/-cr./' | sed 's/.Beta/-beta./' | sed 's/.Alpha/-alpha./'`
-
-sed -i 's/"version": .*/"version": "'$NPM_VERSION'",/' package.json
